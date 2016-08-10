c902efc22b0532442ca8743571539b1bc0a8d7fb
Sui Feng Xu <sfxu@uci.edu>
2016-08-09 20:27:29 -0700

    change the folder name

--- api/test.js
+++ api/test.js
@@ -3,9 +3,8 @@ var re = new RegExp("^(.+)\.(jpg|jpeg|gif|png)$");
 var names = [];
 
 var folder_name = (env.space_path).replace('api/', 'images');
-res.send(folder_name);
-/*
-fs.readdir(__dirname, function(err, files) {
+
+fs.readdir(folder_name, function(err, files) {
     if (err) res.send(err);
     files.forEach(function(f) { 
         if(re.test(f)) {
@@ -15,4 +14,3 @@ fs.readdir(__dirname, function(err, files) {
 
     res.send(names)
 });
-*/
