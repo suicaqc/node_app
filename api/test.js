b1a829958f99fcf03820ff9a142c6c37e5e35853
Sui Feng Xu <sfxu@uci.edu>
2016-08-09 20:26:31 -0700

    env

--- api/test.js
+++ api/test.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var re = new RegExp("^(.+)\.(jpg|jpeg|gif|png)$");
 var names = [];
 
-res.send(env)
+var folder_name = (env.space_path).replace('api/', 'images');
+res.send(folder_name);
 /*
 fs.readdir(__dirname, function(err, files) {
     if (err) res.send(err);
