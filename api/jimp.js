d975eb2a9ffd0ae351739cb49cd22b4a7f940795
Sui Feng Xu <sfxu@uci.edu>
2016-08-11 21:20:41 -0700

    change repository

--- api/jimp.js
+++ api/jimp.js
@@ -1,7 +1,8 @@
-var Jimp = require("package_jimp/node_modules/jimp/");
+var Jimp = require("./package_jimp/node_modules/jimp");
+
+console.log("test1");
 
 var lenna = new Jimp("/var/node_app_server/_microservice/photoserver/photos/IMG_0454.jpg", function () {
     this.resize(180, 180) // resize
         .write("mini.png"); // save
 });
-
