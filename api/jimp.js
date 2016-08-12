e1ae9410e0894ce0fb9ea2541fd1f3be1d1de2c9
Sui Feng Xu <sfxu@uci.edu>
2016-08-11 21:39:45 -0700

    change sending

--- api/jimp.js
+++ api/jimp.js
@@ -1,13 +1,17 @@
 var Jimp = require("/var/node_app_server/_microservice/photoserver/api/package_jimp/node_modules/jimp");
 
 console.log("test1");
-res.send('test1');
+//res.send('test1');
 
 var lenna = new Jimp("/var/node_app_server/_microservice/photoserver/photos/IMG_0454.jpg", function () {
     this.resize(180, 180) // resize
-        .write("mini.png", function() {
-        	res.send('test2');
-        }); // save
+        .write("mini.png", (function(res) {
+        	return function() {
+
+        		res.send('test2');
+        	}
+        	
+        }(res)); // save
 });
 
 
